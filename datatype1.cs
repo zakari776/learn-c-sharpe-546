09436d3dfdf23f4cd5fe846b9adea5dc0d108cbe
zakari776 <hazzouzakaria32@gmail.com>
2025-11-29 19:03:57 +0100

    Uncommented code to display short type max values

---
+++ datatype1.cs
@@ -0,0 +1,92 @@
+//using System;
+
+//class Program
+//{
+//    //static void Main()
+//    //{
+//    //    Console.Write("byte s' MinValue : ");
+//    //    Console.WriteLine(byte.MinValue);   // 0
+//    //    Console.Write("byte s' MaxValue : ");
+//    //    Console.WriteLine(byte.MaxValue);   // 255
+
+//    //    Console.WriteLine();
+
+//    //    Console.Write("sbyte s' MinValue : ");
+//    //    Console.WriteLine(sbyte.MinValue);  // -128
+//    //    Console.Write("sbyte s' MaxValue : ");
+//    //    Console.WriteLine(sbyte.MaxValue);  // 127
+
+//    //    Console.WriteLine();
+
+//    //    Console.Write("short s' MinValue : ");
+//    //    Console.WriteLine(short.MinValue);  // -32768
+//    //    Console.Write("short s' MaxValue : ");
+//    //    Console.WriteLine(short.MaxValue);  // 32767
+
+//    //    Console.WriteLine();
+
+//    //    Console.Write("ushort s' MinValue : ");
+//    //    Console.WriteLine(ushort.MinValue); // 0
+//    //    Console.Write("ushort s' MaxValue : ");
+//    //    Console.WriteLine(ushort.MaxValue); // 65535
+
+//    //    Console.WriteLine();
+
+//    //    Console.Write("int s' MinValue : ");
+//    //    Console.WriteLine(int.MinValue);    // -2147483648
+//    //    Console.Write("int s' MaxValue : ");
+//    //    Console.WriteLine(int.MaxValue);    // 2147483647
+
+//    //    Console.WriteLine();
+
+//    //    Console.Write("uint s' MinValue : ");
+//    //    Console.WriteLine(uint.MinValue);   // 0
+//    //    Console.Write("uint s' MaxValue : ");
+//    //    Console.WriteLine(uint.MaxValue);   // 4294967295
+
+//    //    Console.WriteLine();
+
+//    //    Console.Write("long s' MinValue : ");
+//    //    Console.WriteLine(long.MinValue);   // -9223372036854775808
+//    //    Console.Write("long s' MaxValue : ");
+//    //    Console.WriteLine(long.MaxValue);   // 9223372036854775807
+
+//    //    Console.WriteLine();
+
+//    //    Console.Write("ulong s' MinValue : ");
+//    //    Console.WriteLine(ulong.MinValue);  // 0
+//    //    Console.Write("ulong s' MaxValue : ");
+//    //    Console.WriteLine(ulong.MaxValue);  // 18446744073709551615
+
+//    //    Console.WriteLine();
+
+//    //    Console.Write("float s' MinValue : ");
+//    //    Console.WriteLine(float.MinValue);  // -3.402823E+38
+//    //    Console.Write("float s' MaxValue : ");
+//    //    Console.WriteLine(float.MaxValue);  // 3.402823E+38
+
+//    //    Console.WriteLine();
+
+//    //    Console.Write("double s' MinValue : ");
+//    //    Console.WriteLine(double.MinValue); // -1.7976931348623157E+308
+//    //    Console.Write("double s' MaxValue : ");
+//    //    Console.WriteLine(double.MaxValue); // 1.7976931348623157E+308
+
+//    //    Console.WriteLine();
+
+//    //    Console.Write("decimal s' MinValue : ");
+//    //    Console.WriteLine(decimal.MinValue);// -79228162514264337593543950335M
+//    //    Console.Write("decimal s' MaxValue : ");
+//    //    Console.WriteLine(decimal.MaxValue);// +79228162514264337593543950335M
+
+//    //    Console.WriteLine();
+//    //    Console.WriteLine("Press Enter to close...");
+//    //    Console.ReadLine(); // Keeps window open
+        
+//    //}
+
+//}
+
+Console.WriteLine(Int16.MaxValue);
+Console.WriteLine(short.MaxValue);
+Console.Read();
